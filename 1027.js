57ba32855246c9eca1fc6e367a651dcc8f94c858
johneybi <eun082971@gmail.com>
2025-10-27 09:48:20 +0900

    JS input

---
+++ 1027.js
@@ -0,0 +1,17 @@
+function sum(x, y) {
+    const result = x + y;
+    return result;
+}
+
+sum(3, 5);
+sum(4, 7);
+sum(10, 15)
+
+function sum(x, y) {
+    const result = x + y;
+    console.log(result);
+}
+
+sum(3, 5);
+sum(4, 7);
+sum(10, 15)
