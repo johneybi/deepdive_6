9e5b5590c08d40b812efa1b0b3dc16b89295d85a
johneybi <eun082971@gmail.com>
2025-11-03 10:01:25 +0900

    1103 upload

---
+++ 1103.js
@@ -0,0 +1,13 @@
+
+
+
+function getNumber(num, x) {
+    if (num % x === 0) {
+        console.log(`공배수`)
+    } else {
+        console.log(`공배수 아님`)
+    }
+}
+
+getNumber(60, 3) 
+getNumber(60, 7) 
